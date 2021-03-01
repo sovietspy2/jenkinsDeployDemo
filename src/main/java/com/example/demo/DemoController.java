5f5650b995bb252216126814edfff1d9f6cb85b3
SovietSpy2 <sovietspy2@users.noreply.github.com>
2021-03-01 10:43:28 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping(path = "/test")
+    public String test() {
+        return "Hello there";
+    }
+
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.assertj.core.api.Assertions.*;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DemoApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.get("/test")
+               .accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andReturn();
+    }
+
+    @Test
+    void otherTest() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/test")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String data = result.getResponse().getContentAsString();
+        System.out.println(data);
+        assertThat(data).isEqualTo("Hello there");
+    }
+
+}
